6135b2cb743823e9767097e524a1fff60c6e1ad6
Jay <74105438+weijay0804@users.noreply.github.com>
2021-10-06 23:08:55 +0800

    新增 User 欄位 (email, password_hash), 檢查密碼函式

--- app/model.py
+++ app/model.py
@@ -7,6 +7,8 @@
 
 '''
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 # ----- 自訂函式 -----
 from app import db
 
@@ -18,6 +20,22 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True, index = True)
+    email = db.Column(db.String(128), unique = True, index = True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self) -> None:
+        ''' 讓外不無法直接讀取密碼 '''
+        raise AttributeError('Password is not a readablb attribute.')
+
+    @password.setter
+    def password(self, password : str) -> None:
+        ''' 密碼雜湊化 '''
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password : str) -> bool:
+        ''' 檢查密碼是否正確 '''
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self) -> str:
