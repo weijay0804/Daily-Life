0eb770a5219fb0f8636fb32eb2e65afa67cc4343
Jay <74105438+weijay0804@users.noreply.github.com>
2021-10-07 18:08:05 +0800

    新增 使用者註冊功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,9 @@
     created date : 2021/10/05
     created by : jay
 
+    last update : 2021/10/07
+    update by : jay
+
 '''
 
 from flask import render_template, request, redirect, url_for, session, flash
@@ -17,19 +20,35 @@ from ..model import User
 @auth.route('/register', methods = ['GET', 'POST'])
 def register():
     ''' 使用者註冊 '''
+    
     if request.method == 'POST':
-        username = request.form['username']
-        user = User.query.filter_by(username = username).first()
-        if user:
-            flash('使用者名稱已存在')
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            flash('密碼不相同')
             return redirect(url_for('auth.register'))
-        user = User(username = username)
+        
+        if User.query.filter_by(email = email).first():
+            flash('email 已經被使用')
+            return redirect(url_for('auth.register'))
+
+        if User.query.filter_by(username = username).first():
+            flash('使用者名稱 已經被使用')
+            return redirect(url_for('auth.register'))
+
+        user = User(email = email, username = username, password = password1)
+
         db.session.add(user)
         db.session.commit()
+
         flash('註冊成功')
         return redirect(url_for('main.index'))
-    return render_template('auth/register.html')
 
+    return render_template('auth/register.html')
+    
 @auth.route('/login')
 def login():
     ''' 使用者登入 '''
