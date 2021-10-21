90249d4e7f5b7a8be6a140f248ba43d659851469
Jay <74105438+weijay0804@users.noreply.github.com>
2021-10-21 15:23:57 +0800

    在模板  context 新增  Permission 類別"

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,10 +5,21 @@
     created date : 2021/10/05
     created by : jay
 
+    update date : 2021/10/21
+    update by : jay
+
 '''
 
 from flask import Blueprint
 
+# ----- 自訂函式 -----
+from ..model import Permission
+
 main = Blueprint('main', __name__)
+
+@main.app_context_processor
+def inject_permission():
+    ''' 在模板 context 中加入 Permission類別 '''
+    return dict(Permission = Permission)
 
 from . import views
