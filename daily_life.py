e2ce2027c97f2ef330aca7deee3c213fea45b258
Jay <74105438+weijay0804@users.noreply.github.com>
2021-11-02 16:07:34 +0800

    增加  測試覆蓋率

--- daily_life.py
+++ daily_life.py
@@ -11,21 +11,55 @@
 '''
 
 from flask_migrate import Migrate
+import os
+import sys
+import click
 
 # ----- 自訂函式 -----
 from app import create_app, db
 from app.model import User, Role, Post
 
+COV = None
+# 啟動單元測試覆蓋率
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 app = create_app('default')
 migrate = Migrate(app, db)
+
+
+
+
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db = db, User = User, Role = Role, Post = Post)
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default = False, help = 'Run tests under coverage.')
+def test(coverage):
     ''' 啟動單元測試 '''
+
+    if coverage or not os.environ.get('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))   # 重新啟動程式
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+    # 覆蓋率
+    if COV:
+        COV.stop()
+        COV.save()
+        print('覆蓋率結果: ')
+        COV.report()
+        # 儲存覆蓋率資料
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print(f'HTML version: file://{covdir}//index.html')
+        COV.erase()
